d5852b87986df9409b22cf6056148c27a870fb33
Md. Lutfor Rahman <86643223+lutfor779@users.noreply.github.com>
2022-04-02 01:57:10 +0600

    Revert "antd intial setup and custom theme setup"
    This reverts commit c0c2ee4e4979b64e10dc9c3143e4ac6c4ba00dac.

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
-const withAntdLess = require('next-plugin-antd-less');
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-module.exports = withAntdLess({
-    modifyVars: {
-        '@primary-color': '#f66a05',
-        '@primary-color-hover': '#f24908',
-        '@link-color': 'f66a05',
-        '@link-hover-color': '#f24908',
-        '@link-active-color': '@primary-color',     
-        // input
-        '@border-radius-base': '0.1rem',
-        '@input-placeholder-color': 'rgb(133 148 165)',
-        '@input-border-color': 'rgb(194 201 210)',
-        // button
-        '@btn-font-weight': '500',
-        '@btn-border-radius-base': '0.375rem',      
-    },
-
-    lessVarsFilePathAppendToEndOfContent: false,
-    cssLoaderOptions: {},
-});
+module.exports = nextConfig
