5c6328347db4b19841c3ff56a2d2d223f0848a65
lutfor779 <mdlrrahman779@gmail.com>
2022-04-03 00:12:14 +0600

    initial setup of layout

---
+++ components/shared/footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div className="container mx-auto">
+			Othoba Mart ©2022 Created by Md. Lutfor Rahman
+		</div>
+	);
+};
+
+export default Footer;
