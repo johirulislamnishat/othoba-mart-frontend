261e36b8fcbd400672b296c4513983871c1d43d2
lutfor779 <mdlrrahman779@gmail.com>
2022-04-03 00:12:14 +0600

    initial setup of layout

---
+++ components/home/header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomeHeader = () => {
+	return (
+		<div>
+			<p>This is content</p>
+		</div>
+	);
+};
+
+export default HomeHeader;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: ["./pages/**/*.{js,jsx,html}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./pages/**/*.{js,jsx,html}", "./components/**/*.{js,jsx,html}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
