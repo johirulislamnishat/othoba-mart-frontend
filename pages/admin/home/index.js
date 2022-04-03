356879c33b90e385215dc6ebcc019e7360765d23
lutfor779 <mdlrrahman779@gmail.com>
2022-04-03 14:32:29 +0600

    admin layout initial setup

---
+++ pages/admin/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminLayout from "../../../components/layouts/adminLayout";
+
+const AdminHome = () => {
+	return (
+		<AdminLayout title="Othoba Mart | Admin">
+			This is admin home
+		</AdminLayout>
+	);
+};
+
+export default AdminHome;
