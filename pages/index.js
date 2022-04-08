c8b50aee1014eafa6686d902f49b90f56c04d36e
lutfor779 <mdlrrahman779@gmail.com>
2022-04-09 00:05:49 +0600

    resolve conflicts

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,45 @@
+import { Col, Menu, Row } from "antd";
+import { useState } from "react";
 import HomeHeader from "../components/home/header";
+import NewProducts from "../components/home/NewProducts/NewProducts";
+import ProductCards from "../components/home/ProductCards/ProductCards";
+import PromoLgThree from "../components/home/PromoLgThree/PromoLgThree";
+import PromoLgTwo from "../components/home/PromoLgTwo/PromoLgTwo";
+import Sidebar from "../components/home/Sidebar/Sidebar";
+import useAuth from "../components/hooks/useAuth";
 import HomeLayout from "../components/layouts/homeLayout";
 
+const { SubMenu } = Menu;
+
 export default function Home() {
+	const { firstName, lastName, role } = useAuth();
+	const [current, setCurrent] = useState("mail");
+
+	const handleClick = (e) => {
+		console.log("click ", e);
+		setCurrent(e.key);
+	};
+
 	return (
 		<HomeLayout title="Othoba Mart">
 			<HomeHeader />
+			<NewProducts />
+			<PromoLgTwo />
+			<Row>
+				<Col xs={0} lg={5}>
+					<Sidebar />
+				</Col>
+				<Col
+					xs={16}
+					md={8}
+					lg={19}
+					className="pl-5 sm:pl-32 md:pl-0 lg:pl-8 xl:pl-16"
+				>
+					<NewProducts />
+					<ProductCards />
+				</Col>
+			</Row>
+			<PromoLgThree />
 		</HomeLayout>
 	);
 }
