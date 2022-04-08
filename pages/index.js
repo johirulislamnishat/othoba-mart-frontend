f5a77c741b602e987722f543964bfd22cacd7256
lutfor779 <mdlrrahman779@gmail.com>
2022-04-08 23:13:09 +0600

    antd bug fixed

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
-import { Menu } from "antd";
 import HomeHeader from "../components/home/header";
 import HomeLayout from "../components/layouts/homeLayout";
-
-const { SubMenu } = Menu;
 
 export default function Home() {
 	return (
