c2809f9d34aa3663b3ec59bb260aac94bec6c14a
lutfor779 <mdlrrahman779@gmail.com>
2022-04-17 17:25:50 +0600

    whole file updated

--- components/categories/CategoriesBasedProductsList/CategoriesBasedProductsList.js
+++ components/categories/CategoriesBasedProductsList/CategoriesBasedProductsList.js
@@ -1,92 +1,100 @@
 import {
-  EyeOutlined,
-  HeartOutlined,
-  StarFilled,
-  StarOutlined,
+	EyeOutlined,
+	HeartOutlined,
+	StarFilled,
+	StarOutlined,
 } from "@ant-design/icons";
-import { Col, Row } from "antd";
+import { Col, Image, Row } from "antd";
 import axios from "axios";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { API_BASE_URL } from "../../../apiconstants";
 const CategoriesBasedProductsList = () => {
-  const [items, setItems] = useState([]);
-  console.log(items);
+	const [items, setItems] = useState([]);
+	console.log(items);
 
-  useEffect(() => {
-    axios
-      .get(API_BASE_URL + "/product/paginated?page=0&size=6")
-      .then(function (response) {
-        console.log(response?.data?.result);
-        setItems(response?.data?.result);
-      });
-  }, []);
+	useEffect(() => {
+		axios
+			.get(API_BASE_URL + "/product/paginated?page=0&size=6")
+			.then(function (response) {
+				console.log(response?.data?.result);
+				setItems(response?.data?.result);
+			});
+	}, []);
 
-  return (
-    <div className="container new-products">
-      <Row>
-        {items.map((item, index) => {
-          return (
-            <Col lg={24} key={index}>
-              <div className="product-list-view p-5 flex justify-between items-center border-2 mb-5 rounded-lg">
-                {/* <Image src={item.product_img} alt="" /> */}
+	return (
+		<div className="container new-products">
+			<Row>
+				{items.map((item, index) => {
+					return (
+						<Col lg={24} key={index}>
+							<div className="product-list-view p-5 flex justify-between items-center border-2 mb-5 rounded-lg">
+								{/* <Image src={item.product_img} alt="" /> */}
 
-                <img src={item?.product_img} alt="" />
+								<Image
+									preview={false}
+									src={item?.product_img}
+									alt=""
+								/>
 
-                <div>
-                  <h3>
-                    <Link href="/" className="text-1xl">
-                      {item?.product_name}
-                    </Link>
-                  </h3>
-                  <p>Space for a small product description</p>
-                  <div className="ratings mb-4">
-                    <StarFilled />
-                    <StarFilled />
-                    <StarFilled />
-                    <StarFilled />
-                    <StarOutlined />
-                  </div>
-                  <div className="meta-data">
-                    <p>
-                      <span>Brand:</span>
-                      <span>OPPO</span>
-                    </p>
-                    <p>
-                      <span>Delivery:</span>
-                      <span>Europe</span>
-                    </p>
-                    <p>
-                      <span>Stoke:</span>
-                      <span>320 pcs</span>
-                    </p>
-                  </div>
-                </div>
-                <div className="product-data">
-                  <div className="product-price">
-                    <h3>${item?.product_price}.00</h3>
-                    <small>$779.99</small>
-                  </div>
-                  <h6 className="text-1xl mt-3">Free Shipping</h6>
-                  <p className="text-1xl mt-3">Delivery in 1 day</p>
+								<div>
+									<h3>
+										<Link href="/" className="text-1xl">
+											{item?.product_name}
+										</Link>
+									</h3>
+									<p>Space for a small product description</p>
+									<div className="ratings mb-4">
+										<StarFilled />
+										<StarFilled />
+										<StarFilled />
+										<StarFilled />
+										<StarOutlined />
+									</div>
+									<div className="meta-data">
+										<p>
+											<span>Brand:</span>
+											<span>OPPO</span>
+										</p>
+										<p>
+											<span>Delivery:</span>
+											<span>Europe</span>
+										</p>
+										<p>
+											<span>Stoke:</span>
+											<span>320 pcs</span>
+										</p>
+									</div>
+								</div>
+								<div className="product-data">
+									<div className="product-price">
+										<h3>${item?.product_price}.00</h3>
+										<small>$779.99</small>
+									</div>
+									<h6 className="text-1xl mt-3">
+										Free Shipping
+									</h6>
+									<p className="text-1xl mt-3">
+										Delivery in 1 day
+									</p>
 
-                  <button className="custom-btn">
-                    <EyeOutlined />
-                    <span>Product Details </span>
-                  </button>
+									<button className="custom-btn">
+										<EyeOutlined />
+										<span>Product Details </span>
+									</button>
 
-                  <button className="custom-btn grey-btn">
-                    <HeartOutlined />
-                    <span> Add To Wishlist</span>
-                  </button>
-                </div>
-              </div>
-            </Col>
-          );
-        })}
-      </Row>
-    </div>
-  );
+									<button className="custom-btn grey-btn">
+										<HeartOutlined />
+										<span> Add To Wishlist</span>
+									</button>
+								</div>
+							</div>
+						</Col>
+					);
+				})}
+			</Row>
+		</div>
+	);
 };
 
 export default CategoriesBasedProductsList;
--- pages/product/[id]/index.js
+++ pages/product/[id]/index.js
@@ -1,11 +1,11 @@
 import {
-  EyeOutlined,
-  HeartOutlined,
-  QuestionCircleOutlined,
-  StarFilled,
-  StarOutlined,
+	EyeOutlined,
+	HeartOutlined,
+	QuestionCircleOutlined,
+	StarFilled,
+	StarOutlined,
 } from "@ant-design/icons";
-import { Col, InputNumber, Row, Tabs } from "antd";
+import { Col, Image, InputNumber, Row, Tabs } from "antd";
 import axios from "axios";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
@@ -13,119 +13,123 @@ import { API_BASE_URL } from "../../../apiconstants";
 import HomeLayout from "../../../components/layouts/homeLayout";
 import RelateProducts from "../../../components/RelatedProducts/RelateProducts";
 const Product = () => {
-  const router = useRouter();
-  const { id } = router.query;
+	const router = useRouter();
+	const { id } = router.query;
 
-  const [item, setItem] = useState([]);
+	const [item, setItem] = useState([]);
 
-  useEffect(() => {
-    axios.get(API_BASE_URL + `/product/${id}`).then(function (response) {
-      setItem(response?.data?.result);
-    });
-  }, [id]);
-  function onChange(value) {
-    console.log("changed", value);
-  }
-  const { TabPane } = Tabs;
-  function callback(key) {
-    console.log(key);
-  }
-  const url = item.product_img;
-  console.log(url);
-  return (
-    <HomeLayout title={item.product_name}>
-      <div className="single-product-page">
-        <Row>
-          <Col sm={12} gutter={8} className="border rounded-md">
-            <img src={url} alt="" />
-          </Col>
-          <Col sm={12} className="pl-6">
-            <h2>{item.product_name}</h2>
-            <div className="ratings mt-4">
-              <StarFilled />
-              <StarFilled />
-              <StarFilled />
-              <StarFilled />
-              <StarOutlined />
-              <span>1 customer review</span>
-            </div>
-            <p className="mt-6">{item.product_description}</p>
-            <table className="mt-6">
-              <tbody>
-                <tr>
-                  <td>SKU</td>
-                  <td>4353453453</td>
-                  <td>Freshness</td>
-                  <td>1 day old</td>
-                </tr>
-                <tr>
-                  <td>Category</td>
-                  <td>{item?.product_category}</td>
-                  <td>Tags</td>
-                  <td>{item?.product_tags}</td>
-                </tr>
-                <tr>
-                  <td>Stock</td>
-                  <td>In Stock</td>
-                  <td>Delivery</td>
-                  <td>In 2 days</td>
-                </tr>
-                <tr>
-                  <td>Colors</td>
-                  <td>{item?.product_colors}</td>
-                  <td>Sizes</td>
-                  <td>{item?.product_sizes}</td>
-                </tr>
-              </tbody>
-            </table>
-            <div className="product-data flex justify-between items-center border p-3 my-6  rounded-lg">
-              <div className="product-price">
-                <h3>
-                  <span>$</span>
-                  {item.product_price}
-                </h3>
-                <small>$779.99</small>
-              </div>
-              <InputNumber
-                size="large"
-                min={1}
-                max={100000}
-                defaultValue={3}
-                onChange={onChange}
-              />
-              <button className="custom-btn">
-                <EyeOutlined />
-                <span>Add To Cart</span>
-              </button>
+	useEffect(() => {
+		axios.get(API_BASE_URL + `/product/${id}`).then(function (response) {
+			setItem(response?.data?.result);
+		});
+	}, [id]);
+	function onChange(value) {
+		console.log("changed", value);
+	}
+	const { TabPane } = Tabs;
+	function callback(key) {
+		console.log(key);
+	}
+	const url = item.product_img;
+	console.log(url);
+	return (
+		<HomeLayout title={item.product_name}>
+			<div className="single-product-page">
+				<Row>
+					<Col sm={12} gutter={8} className="border rounded-md">
+						<Image preview={false} src={url} alt="" />
+					</Col>
+					<Col sm={12} className="pl-6">
+						<h2>{item.product_name}</h2>
+						<div className="ratings mt-4">
+							<StarFilled />
+							<StarFilled />
+							<StarFilled />
+							<StarFilled />
+							<StarOutlined />
+							<span>1 customer review</span>
+						</div>
+						<p className="mt-6">{item.product_description}</p>
+						<table className="mt-6">
+							<tbody>
+								<tr>
+									<td>SKU</td>
+									<td>4353453453</td>
+									<td>Freshness</td>
+									<td>1 day old</td>
+								</tr>
+								<tr>
+									<td>Category</td>
+									<td>{item?.product_category}</td>
+									<td>Tags</td>
+									<td>{item?.product_tags}</td>
+								</tr>
+								<tr>
+									<td>Stock</td>
+									<td>In Stock</td>
+									<td>Delivery</td>
+									<td>In 2 days</td>
+								</tr>
+								<tr>
+									<td>Colors</td>
+									<td>{item?.product_colors}</td>
+									<td>Sizes</td>
+									<td>{item?.product_sizes}</td>
+								</tr>
+							</tbody>
+						</table>
+						<div className="product-data flex justify-between items-center border p-3 my-6  rounded-lg">
+							<div className="product-price">
+								<h3>
+									<span>$</span>
+									{item.product_price}
+								</h3>
+								<small>$779.99</small>
+							</div>
+							<InputNumber
+								size="large"
+								min={1}
+								max={100000}
+								defaultValue={3}
+								onChange={onChange}
+							/>
+							<button className="custom-btn">
+								<EyeOutlined />
+								<span>Add To Cart</span>
+							</button>
 
-              <button className="custom-btn grey-btn">
-                <HeartOutlined />
-                <span> Add To Wishlist</span>
-              </button>
-            </div>
+							<button className="custom-btn grey-btn">
+								<HeartOutlined />
+								<span> Add To Wishlist</span>
+							</button>
+						</div>
 
-            <Tabs className="mt-6" defaultActiveKey="1" onChange={callback}>
-              <TabPane tab="Descriptions" key="1">
-                <p>{item.product_description}</p>
-              </TabPane>
-              <TabPane tab="Reviews" key="2">
-                No Reviews Available
-              </TabPane>
-              <TabPane tab="Questions" key="3">
-                <button className="custom-btn">
-                  <QuestionCircleOutlined />
-                  <span>Ask Question</span>
-                </button>
-              </TabPane>
-            </Tabs>
-          </Col>
-        </Row>
-        <Row>
-          <RelateProducts></RelateProducts>
-        </Row>
-      </div>
-    </HomeLayout>
-  );
+						<Tabs
+							className="mt-6"
+							defaultActiveKey="1"
+							onChange={callback}
+						>
+							<TabPane tab="Descriptions" key="1">
+								<p>{item.product_description}</p>
+							</TabPane>
+							<TabPane tab="Reviews" key="2">
+								No Reviews Available
+							</TabPane>
+							<TabPane tab="Questions" key="3">
+								<button className="custom-btn">
+									<QuestionCircleOutlined />
+									<span>Ask Question</span>
+								</button>
+							</TabPane>
+						</Tabs>
+					</Col>
+				</Row>
+				<Row>
+					<RelateProducts></RelateProducts>
+				</Row>
+			</div>
+		</HomeLayout>
+	);
 };
 
 export default Product;
