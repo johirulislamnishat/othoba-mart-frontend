e558d4bbd58e1ba0bc0876981eb2981f51c2a4bd
lutfor779 <mdlrrahman779@gmail.com>
2022-04-23 23:10:35 +0600

    bug fixed

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { HomeLayout } from "../../components/layouts/homeLayout";
+import HomeLayout from "../../components/layouts/homeLayout";
 
 const About = () => {
-  return <HomeLayout title="About Us | Othoba Mart"></HomeLayout>;
+	return <HomeLayout title="About Us | Othoba Mart"></HomeLayout>;
 };
 
 export default About;
