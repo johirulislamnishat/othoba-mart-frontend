d3aaf46deafea029a39e4355fe495151896bbfdb
jalishmahmud <jalish93@gmail.com>
2022-04-25 23:01:35 +0600

    checked with yarn build and yarn start

--- components/home/PromoSmOne/PromoSmOne.js
+++ components/home/PromoSmOne/PromoSmOne.js
@@ -4,7 +4,7 @@ import headphone from "../../../assets/Images/headphone.png";
 import speaker from "../../../assets/Images/speaker.png";
 const PromoSmOne = () => {
   return (
-    <div className="container promo-sm-one ">
+    <div className="container promo-sm-one">
       <Row>
         <Col md={12} sm={24}>
           <Row className="promo-lg bg-gray-100 p-6 md:mr-2 mb-4 flex justify-between items-center">
