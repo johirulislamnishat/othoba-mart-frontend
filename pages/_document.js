8ed0195438e542f82d1d8b13d4403028e8cf3bea
jalishmahmud <jalish93@gmail.com>
2022-05-17 18:32:45 +0600

    dark theme working for but still have some issue

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,51 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: themeInitializerScript,
+          }}
+        ></script>
         <Main />
         <NextScript />
       </body>
     </Html>
   );
+}
+
+// This function needs to be a String
+const themeInitializerScript = `(function() {
+	${setInitialColorMode.toString()}
+	setInitialColorMode();
+})()
+`;
+
+function setInitialColorMode() {
+  // Check initial color preference
+  function getInitialColorMode() {
+    const persistedPreferenceMode = window.localStorage.getItem("theme");
+    const hasPersistedPreference = typeof persistedPreferenceMode === "string";
+
+    if (hasPersistedPreference) {
+      return persistedPreferenceMode;
+    }
+
+    // Check the current preference
+    const preference = window.matchMedia("(prefers-color-scheme: dark)");
+    const hasMediaQueryPreference = typeof preference.matches === "boolean";
+
+    if (hasMediaQueryPreference) {
+      return preference.matches ? "dark" : "light";
+    }
+
+    return "light";
+  }
+
+  const currentColorMode = getInitialColorMode();
+  const element = document.documentElement;
+  element.style.setProperty("--initial-color-mode", currentColorMode);
+
+  // If darkmode apply darkmode
+  if (currentColorMode === "dark")
+    document.documentElement.setAttribute("data-theme", "dark");
 }
