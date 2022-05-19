20ee2f6ac9b77e2ed28826257f754ec6601b3947
jalishmahmud <jalish93@gmail.com>
2022-05-19 23:58:29 +0600

    High to low and low to high working

--- components/categories/CategoriesBasedProductsGrid/CategoriesBasedProductsGrid.js
+++ components/categories/CategoriesBasedProductsGrid/CategoriesBasedProductsGrid.js
@@ -1,4 +1,4 @@
-import { Col, Row } from "antd";
+import { Col, Radio, Row } from "antd";
 import axios from "axios";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
@@ -8,19 +8,51 @@ import ProductCardGrid from "../../ProductCardGrid/ProductCardGrid";
 const CategoriesBasedProductsGrid = ({ selectedCategory }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [items, setItems] = useState([]);
+  const [value, setValue] = useState("1");
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    var products = items.sort((a, b) => {
+      return value == "1"
+        ? a.product_price - b.product_price
+        : b.product_price - a.product_price;
+    });
+    setItems(products);
+  }, [value]);
 
   useEffect(() => {
     setIsLoading(true);
     axios
       .get(API_BASE_URL + `/product?cat=${selectedCategory}`)
       .then(function (response) {
-        setItems(response?.data?.result);
+        let products = response?.data?.result;
+        products = products.sort((a, b) => {
+          return b.product_price - a.product_price;
+        });
+        setItems(products);
         setIsLoading(false);
       });
   }, [selectedCategory]);
 
+  useEffect(() => {
+    console.log(items);
+  }, []);
+
   return (
     <div className="container category-based-products">
+      <Radio.Group
+        style={{ marginBottom: 30 }}
+        onChange={onChange}
+        value={value}
+      >
+        <Radio defaultChecked={false} value={"1"}>
+          High to Low
+        </Radio>
+        <Radio value={"2"}>Low to High</Radio>
+      </Radio.Group>
       {isLoading && (
         <Image
           height="40"
