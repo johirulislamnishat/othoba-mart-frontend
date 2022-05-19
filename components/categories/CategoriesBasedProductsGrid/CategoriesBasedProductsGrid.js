3388b85cb6aa80348bcb4fd90bbf9a9ff139875a
jalishmahmud <jalish93@gmail.com>
2022-05-19 22:27:25 +0600

    preloader added

--- components/categories/CategoriesBasedProductsGrid/CategoriesBasedProductsGrid.js
+++ components/categories/CategoriesBasedProductsGrid/CategoriesBasedProductsGrid.js
@@ -1,22 +1,35 @@
 import { Col, Row } from "antd";
 import axios from "axios";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { API_BASE_URL } from "../../../apiconstants";
+import preloader from "../../../assets/Images/preeloader.gif";
 import ProductCardGrid from "../../ProductCardGrid/ProductCardGrid";
-
 const CategoriesBasedProductsGrid = ({ selectedCategory }) => {
+  const [isLoading, setIsLoading] = useState(false);
   const [items, setItems] = useState([]);
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get(API_BASE_URL + `/product?cat=${selectedCategory}`)
       .then(function (response) {
         setItems(response?.data?.result);
+        setIsLoading(false);
       });
   }, [selectedCategory]);
 
   return (
     <div className="container category-based-products">
+      {isLoading && (
+        <Image
+          height="40"
+          width="40"
+          className="preloader"
+          src={preloader}
+          alt=""
+        />
+      )}
       <Row>
         {items.map((item, index) => {
           return (
