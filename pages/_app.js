2381b6e02e1f497e3e403384cc9c52199c446608
lutfor779 <mdlrrahman779@gmail.com>
2022-05-24 17:07:11 +0600

    login logout issue fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
+import AuthProvider from "../context/AuthProvider";
 import ContextProvider from "../context/ContextProvider";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ContextProvider>
-      <Component {...pageProps} />
-    </ContextProvider>
-  );
+	return (
+		<ContextProvider>
+			<AuthProvider>
+				<Component {...pageProps} />
+			</AuthProvider>
+		</ContextProvider>
+	);
 }
 
 export default MyApp;
